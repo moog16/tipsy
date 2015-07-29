2c983cf05075edbc630b1c0f2b481f28884e11eb
Matt Goo <mattgoo@gmail.com>
2015-07-29 14:11:31 -0700

    created settings modal, and able to open

--- tipsy/ViewController.swift
+++ tipsy/ViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var billLabel: UITextField!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsIcon: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        if let billLabelDefault = defaults.stringForKey("billAmount") {
+            billLabel.text = billLabelDefault
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -26,8 +32,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onBillAmountEdit(sender: AnyObject) {
+        let defaults = NSUserDefaults.standardUserDefaults()
         var billAmountText = billLabel.text
         var billAmount = (billAmountText as NSString).doubleValue
+        defaults.setObject(billAmountText, forKey: "billAmount")
+        
         var tipPercentages = [0.15, 0.18, 0.2, 0.22]
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         var tipAmount = billAmount * tipPercentage
@@ -41,6 +50,8 @@ class ViewController: UIViewController {
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
-
+    @IBAction func settingsButtonPressed(sender: AnyObject) {
+        performSegueWithIdentifier("settingsPanel", sender: self)
+    }
 }
 
