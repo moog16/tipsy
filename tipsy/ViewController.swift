a96d13ab7cfdb721bdf389676f05d844f3bae8bd
Matt Goo <mattgoo@gmail.com>
2015-07-29 13:03:18 -0700

    have working tipsy calculator

--- tipsy/ViewController.swift
+++ tipsy/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billLabel: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onBillAmountEdit(sender: AnyObject) {
+        var billAmountText = billLabel.text
+        var billAmount = (billAmountText as NSString).doubleValue
+        var tipPercentages = [0.15, 0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var tipAmount = billAmount * tipPercentage
+        var totalAmount = billAmount + tipAmount
+        
+        tipLabel.text = String(format: "$%.2f", tipAmount)
+        totalLabel.text = String(format: "$%.2f", totalAmount)
+        
+
+    }
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
 }
 
